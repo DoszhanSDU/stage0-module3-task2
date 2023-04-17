faccc45d254870233e6f690398e17e19636729da
DosyaSDU <doszhan_turar@mail.ru>
2023-04-17 17:03:26 +0600

    fixed

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable1 = 10;
-        int variableOne1 = 1;
-        int variableTwo1 = 2;
-        int variableThree1 = 3;
-        final int VARIABLE_FOUR1 = 4;
-        final int VARIABLE_FIVE1 = 5;
-        int variableSix1 = 6;
-        int variableSeven1 = 7;
-        int variableEight1 = 8;
+        int variableTen = 10;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
